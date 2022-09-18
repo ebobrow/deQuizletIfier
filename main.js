0103e8162893fb36cc67240c26c0554d28f0061f
Elliot Bobrow <elliotgreybobrow@gmail.com>
2022-09-17 18:17:37 -0700

    basic functionality

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
-let curSet;
-let curAnswer;
+let curCards;
+let ptr = 0;
 
-inputSet = () => {
-    const setEl = document.getElementById("set");
-    curSet = setEl.value;
-    setEl.value = "";
-    curSet = curSet.split("\n").map(line => {
-        const [term, definition] = line.split("	");
-        return { term: term, definition: definition };
-    });
-    console.log(curSet);
-    const prompt = document.getElementById("prompt");
-    prompt.innerHTML = curSet[0].definition;
-    curAnswer = curSet[0].term;
-}
+const setPrompt = val => {
+  const prompt = document.getElementById("prompt");
+  prompt.innerHTML = val;
+};
+
+const setCards = () => {
+  const cardsEl = document.getElementById("cards");
+  curCards = cardsEl.value;
+  cardsEl.value = "";
+  curCards = curCards.split("\n").map(line => {
+    const [term, definition] = line.split("	");
+    return { term, definition };
+  });
+  curCards.sort(() => Math.random() - 0.5);
+  ptr = 0;
+  setPrompt(curCards[ptr].definition);
+};
+
+const guess = () => {
+  const guessEl = document.getElementById("guess");
+  const guess = guessEl.value;
+  guessEl.value = "";
+  if (guess === curCards[ptr].term) {
+    setPrompt("Correct!");
+  } else {
+    setPrompt(`Incorrect; answer was ${curCards[ptr].term}`);
+  }
+  setTimeout(newCard, 1000);
+};
+
+const newCard = () => {
+  ptr += 1;
+  setPrompt(curCards[ptr].definition);
+};
